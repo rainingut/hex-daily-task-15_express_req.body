00bad7a879a1f9b9069e7e6b96ab55fd48172b92
rainingut <keeprainingut@gmail.com>
2022-05-04 09:28:55 +0800

    新增model 新增 post 方法

--- models/posts.js
+++ models/posts.js
@@ -1,6 +1,32 @@
 const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema(
   /* 加入欄位驗證 */
+  {
+    title : {
+      type: String,
+      required: [true, `標題必填`]
+    },
+    content: {
+      type: String,
+      required: [true, '內文必填']
+    },
+    likes: {
+      type: Number,
+      default: 0
+    },
+    comments: {
+      type: Number,
+      default: 0
+    },
+    tags: {
+      type: [String],
+      default: []
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }
 );
 
 const Post = mongoose.model('Post', postSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -6,9 +6,20 @@ router.post('/', async(req, res, next) =>  {
   try {
       /* 請在此填寫答案 */
       // 取得來自 request body 的資料
-      
+      const data = req.body;
       // 驗證是否有 content 欄位 -> 若有則使用 mongoose 語法新增資料 -> 回傳成功回應
       //                       -> 未填寫 content 欄位 -> 回傳失敗回應
+      if (!data.title.trim() || !data.content.trim()) {
+        res.status(400).send({status: false, message: `標題與內容必填`});
+        return;
+      }
+      else {
+        const posts = await Post.find();
+        await Post.create({...data})
+        .then(() => res.send({status: true, message: '新增成功', data: posts}))
+        .catch((error) => res.status(400).send({status: true, message: '新增失敗', error}));
+        
+      }
   
   } catch (error) {
     res.status(400).json({
